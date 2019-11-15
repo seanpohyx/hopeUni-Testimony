ae0ec4ea3d1c19265444dd98e6a734eafc3ac5c2
seanpohyx <seanpohyx@gmail.com>
2019-11-16 05:07:03 +0800

    managed to display, but not linked with prayer warrior yet

--- script/examWall.js
+++ script/examWall.js
@@ -1,14 +1,14 @@
 var data = {
 	"exams" : {
 		'29/11/2019 09:00': [
-			{id: 1, code_code: "CZ2001", course_name: "PRINCIPLES OF NUTRITION", place: "NIE", lifegroup: 1, contact: 92721782, request: ""},	
-			{id: 2, code_code: "CZ2001", course_name: "PRINCIPLES OF NUTRITION", place: "NIE", lifegroup: 1, contact: 92721782, request: ""},
-			{id: 3, code_code: "CZ2001", course_name: "PRINCIPLES OF NUTRITION", place: "NIE", lifegroup: 1, contact: 92721782, request: ""}
+			{id: 1, name: "viena", code_code: "CZ2001", course_name: "PRINCIPLES OF NUTRITION", place: "NIE", lifegroup: 1, contact: 92721782, request: ""},	
+			{id: 2, name: "viena", code_code: "CZ2001", course_name: "PRINCIPLES OF NUTRITION", place: "NIE", lifegroup: 1, contact: 92721782, request: ""},
+			{id: 3, name: "viena", code_code: "CZ2001", course_name: "PRINCIPLES OF NUTRITION", place: "NIE", lifegroup: 1, contact: 92721782, request: ""}
 		],
 		'30/11/2019 09:00': [
-			{id: 4, code_code: "CZ2001", course_name: "PRINCIPLES OF NUTRITION", place: "NIE", lifegroup: 1, contact: 92721782, request: ""},	
-			{id: 5, code_code: "CZ2001", course_name: "PRINCIPLES OF NUTRITION", place: "NIE", lifegroup: 1, contact: 92721782, request: ""},
-			{id: 6, code_code: "CZ2001", course_name: "PRINCIPLES OF NUTRITION", place: "NIE", lifegroup: 1, contact: 92721782, request: ""}
+			{id: 4, name: "viena", code_code: "CZ2001", course_name: "PRINCIPLES OF NUTRITION", place: "NIE", lifegroup: 1, contact: 92721782, request: ""},	
+			{id: 5, name: "viena", code_code: "CZ2001", course_name: "PRINCIPLES OF NUTRITION", place: "NIE", lifegroup: 1, contact: 92721782, request: ""},
+			{id: 6, name: "viena", code_code: "CZ2001", course_name: "PRINCIPLES OF NUTRITION", place: "NIE", lifegroup: 1, contact: 92721782, request: ""}
 		]
 
 	},
@@ -26,52 +26,53 @@ $( document ).ready(function() {
 });
 
 function retrievePrayerWall(){
-	// var Mark = "mark";
-	// $( "#exam-content" ).html("");
-	// Object.keys(data.exams).forEach(function (item) {
-	// 	// console.log(item); // key
-	// 	// console.log(course_code_mapping[item]); // value
-	// 	console.log(data.exams[item])
-	// 	$( "#exam-content" ).append(
-	// 		'<div class="card mb-3 col-md-12">'
-	//         +'<div class="card-header">'
-	//         + "29/12/19 1am"
-	//         +'</div>'
-	//         +'<div class="card-body">'
-	//         +'<table class="table">'
-	//         +'<thead>'
-	//         +'<tr>'
-	//         +'<th scope="col">Examinee</th>'
-	//         +'<th scope="col">Course</th>'
-	//         +'<th scope="col">LG</th>'
-	//         +'<th scope="col">Venue</th>'
-	//         +'<th scope="col">Contact No</th>'
-	//         +'<th scope="col">Special Request</th>'
-	//         +'<th scope="col">Prayer Warrior</th>'
-	//         +'</tr>'
-	//         +'</thead>'
-	//         +'<tbody>');
-	// 	for(var i=0; i<data.exams[item].length; i++){
+	var Mark = "mark";
+	var cardStr = "";
+	$( "#exam-content" ).html("");
 
-	// 		console.log(data.exams[item][i])
-	// 		$( "#exam-content" ).append(
-	//         '<tr>'
-	//         +'<td>'+Mark+'</td>'
-	//         +'<td>'+Mark+'</td>'
-	//         +'<td>'+Mark+'</td>'
-	//         +'<td>'+Mark+'</td>'
-	//         +'<td>'+Mark+'</td>'
-	//         +'<td>'+Mark+'</td>'
-	//         +'<td>'+Mark+'</td>'
-	//         +'</tr>');
+	Object.keys(data.exams).forEach(function (item) {
+		// console.log(item); // key
+		// console.log(course_code_mapping[item]); // value
+		console.log(item)
+		cardStr += '<div class="card mb-3 col-md-12">'
+	        +'<div class="card-header">'
+	        + item
+	        +'</div>'
+	        +'<div class="card-body">'
+	        +'<table class="table">'
+	        +'<thead>'
+	        +'<tr>'
+	        +'<th scope="col">Examinee</th>'
+	        +'<th scope="col">Course</th>'
+	        +'<th scope="col">LG</th>'
+	        +'<th scope="col">Venue</th>'
+	        +'<th scope="col">Contact No</th>'
+	        +'<th scope="col">Special Request</th>'
+	        +'<th scope="col">Prayer Warrior</th>'
+	        +'</tr>'
+	        +'</thead>'
+	        +'<tbody>';
+		for(var i=0; i<data.exams[item].length; i++){
 
-	// 	}
-	// 	$( "#exam-content" ).append(
-	//         '</tbody>'
-	//         +'</table>'
-	//         +'</div>'
-	//         +'</div>');
-	// });
+			cardStr += '<tr>'
+	        +'<td>'+data.exams[item][i].name+'</td>'
+	        +'<td>'+data.exams[item][i].course_name+'</td>'
+	        +'<td>'+data.exams[item][i].lifegroup+'</td>'
+	        +'<td>'+data.exams[item][i].place+'</td>'
+	        +'<td>'+data.exams[item][i].contact+'</td>'
+	        +'<td>'+data.exams[item][i].request+'</td>'
+	        +'<td>'+"Mark"+'</td>' //havent link with prayer id
+	        +'</tr>';
+
+		}
+		cardStr += 
+	        '</tbody>'
+	        +'</table>'
+	        +'</div>'
+	        +'</div>';
+
+		$( "#exam-content" ).append(cardStr);
+	});
 
 
 }
