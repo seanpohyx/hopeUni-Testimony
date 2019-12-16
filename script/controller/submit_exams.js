1191d0d408e74e777b68e782c0746520010533e5
seanpohyx <seanpohyx@gmail.com>
2019-12-16 15:07:23 +0800

    ui for submit exams

--- script/controller/submit_exams.js
+++ script/controller/submit_exams.js
@@ -8,6 +8,34 @@ $( document ).ready(function() {
 	var contactNo = $('#contactTB')[0];
 	//var api_link = "";
 
+	var course_code_search = new Bloodhound({
+		local: course_code_json,
+		datumTokenizer: Bloodhound.tokenizers.obj.whitespace("course_code","course_name"),
+		queryTokenizer: Bloodhound.tokenizers.whitespace
+	});
+	course_code_search.initialize();
+
+	var elt = $('#course_input_div > > input');
+
+	elt.tagsinput({
+		itemValue: 'course_code',
+		itemText: 'course_code',
+		typeaheadjs: {
+			name: 'course_code_search',
+			displayKey: 'course_code',
+			display: 'course_name',
+			limit: 10,
+			source: course_code_search.ttAdapter()
+		}
+	});
+
+	$('#course_input_div').on('keydown', function(e) {
+	    if (e.keyCode === 13) {
+	    	console.log("enter")
+	        return false
+	    }
+	});
+	
 	$.ajax({
 			url: api_link+"api/Lg/" + campus_id,
 			type: "GET",
@@ -26,18 +54,20 @@ $( document ).ready(function() {
 			}
 		});
 
-	Object.keys(course_code_mapping).forEach(function (item) {
-		// console.log(item); // key
-		// console.log(course_code_mapping[item]); // value
-		$("#course_code_SelectInput").append("<option value='"+item+"'>"+item+"</option>");
+	// Object.keys(course_code_mapping).forEach(function (item) {
+	// 	// console.log(item); // key
+	// 	// console.log(course_code_mapping[item]); // value
+	// 	$("#course_code_SelectInput").append("<option value='"+item+"'>"+item+"</option>");
 
-	});
+	// });
 
 	isSpinner(false);
 
 	$('#verification-btn').on('click',{},(event) =>{
 
-		if(nameStr.value== "" || courseCodeInput.value == "" || parseInt(lgInt.value) == 0){
+		var courseCodeInput = $('#course_input_div > > input').tagsinput('items');
+
+		if(nameStr.value== "" || courseCodeInput.length == 0 || parseInt(lgInt.value) == 0){
 
 			modal.find('.modal-body').html('Please fill in all the blanks');
 			modal.find('.modal-title ').html('Error');
@@ -51,7 +81,24 @@ $( document ).ready(function() {
 			modal.modal('show');
 		}
 		else{
-			modal.find('.modal-body').html('Are you sure?');
+			var msg = '<ul>';
+
+			msg += '<li><p>Name: <span class="font-weight-bold">' + nameStr.value + '</span><br>'
+				+ 'Contact No: <span class="font-weight-bold">' + parseInt(contactNo.value) + '</span><br>';
+
+			msg += (specialRequestStr.value != '')?'Special Request: <span class="font-weight-bold">' + specialRequestStr.value + '</span></p></li>':'</p></li>';
+
+			for(var i=0; i<courseCodeInput.length;i++){
+
+				msg += '<li><p><span class="font-weight-bold">' + courseCodeInput[i].course_name + '</span>'
+					+ '<span class="text-monospace text-secondary">[' + courseCodeInput[i].course_code + ']</span><br>'
+					+ 'Date & Time: '+courseCodeInput[i].datetime+'<br>'
+					+ 'Venue: '+courseCodeInput[i].place+'</li>';
+			}
+
+			msg += '</ul>';
+
+			modal.find('.modal-body').html(msg);
 			modal.find('.modal-title ').html('Confirmation');
 			$('.modal-footer .btn-custom-blue').css('visibility', 'visible');
 			modal.modal('show');
@@ -60,38 +107,41 @@ $( document ).ready(function() {
 
 	$('#submit').on('click',{},(event) =>{
 
-		isSpinner(true);
-		$.ajax({
-				url: api_link+"api/testimonies",
-				type: "POST",
-				crossDomain: true,
-				async: false,
-				dataType: "json",
-				data: JSON.stringify( {
-					"name": nameStr.value,
-					"place": (course_code_mapping[courseCodeInput.value]).place,
-					"time": (course_code_mapping[courseCodeInput.value]).datetime,
-					"course_code": courseCodeInput.value,
-					"course_name": (course_code_mapping[courseCodeInput.value]).course_name,
-					"lg": parseInt(lgInt.value),
-					"contact": parseInt(contactNo.value),
-					"request": specialRequestStr.value
 
-				}),
-				contentType: 'application/json',
-				success: function (response) {
-					isSpinner(false);
-					window.location="index";
-				},
-				error: function (xhr, status) {
-					modal.find('.modal-body').html('<p>Sorry, there is a bug with the website, it will be fixed shortly. <a href="'+website_feedback+'">Report Bug</a></p>');
-					modal.find('.modal-title ').html('Error');
-					$('.modal-footer .btn-custom-blue').css('visibility', 'hidden');
-					modal.modal('show');
-					isSpinner(false);
-				},
+		console.log($("input").val());
 
-			});
+		// isSpinner(true);
+		// $.ajax({
+		// 		url: api_link+"api/testimonies",
+		// 		type: "POST",
+		// 		crossDomain: true,
+		// 		async: false,
+		// 		dataType: "json",
+		// 		data: JSON.stringify( {
+		// 			"name": nameStr.value,
+		// 			"place": (course_code_mapping[courseCodeInput.value]).place,
+		// 			"time": (course_code_mapping[courseCodeInput.value]).datetime,
+		// 			"course_code": courseCodeInput.value,
+		// 			"course_name": (course_code_mapping[courseCodeInput.value]).course_name,
+		// 			"lg": parseInt(lgInt.value),
+		// 			"contact": parseInt(contactNo.value),
+		// 			"request": specialRequestStr.value
+
+		// 		}),
+		// 		contentType: 'application/json',
+		// 		success: function (response) {
+		// 			isSpinner(false);
+		// 			window.location="index";
+		// 		},
+		// 		error: function (xhr, status) {
+		// 			modal.find('.modal-body').html('<p>Sorry, there is a bug with the website, it will be fixed shortly. <a href="'+website_feedback+'">Report Bug</a></p>');
+		// 			modal.find('.modal-title ').html('Error');
+		// 			$('.modal-footer .btn-custom-blue').css('visibility', 'hidden');
+		// 			modal.modal('show');
+		// 			isSpinner(false);
+		// 		},
+
+		// 	});
 
 	});
 
---
+++ script/controller/submit_testimony.js
@@ -0,0 +1,83 @@
+$( document ).ready(function() {
+
+	var modal = $('#confirmationModal');
+	var nameStr = $('#nameTB')[0];
+	var testimonyStr = $('#testimonyTA')[0];
+	var lgInt = $('#lg-grpselectInput')[0];
+
+	$.ajax({
+		url: api_link+"api/Lg/" + campus_id,
+		type: "GET",
+		crossDomain: true,
+		async: false,
+		dataType: "json",
+		contentType: 'application/json',
+		success: function (response) {
+			$.each(response, function( index, value ) {
+				$("#lg-grpselectInput").append("<option value='"+value.id+"'>"+value.lg+"</option>");
+			})
+		},
+		error: function (xhr, status) {
+			console.log(xhr);
+			alert("No LG, contact admin");
+		}
+	});
+
+	$('#verification-btn').on('click',{
+	}, (event) => {
+
+		if(nameStr.value== "" || testimonyStr.value == "" || parseInt(lgInt.value) == 0){
+
+			modal.find('.modal-body').html('Please fill in all the blanks');
+			modal.find('.modal-title ').html('Error');
+			$('.modal-footer .btn-custom-blue').css('visibility', 'hidden');
+			modal.modal('show');
+		}
+		else{
+			modal.find('.modal-body').html('Are you sure?');
+			modal.find('.modal-title ').html('Confirmation');
+			$('.modal-footer .btn-custom-blue').css('visibility', 'visible');
+			modal.modal('show');
+		}
+
+	});
+
+	$('#submit').on('click',{
+	}, (event) => {
+
+		isSpinner(true);
+
+		$.ajax({
+			url: api_link+"api/testimonies",
+			type: "POST",
+			crossDomain: true,
+			async: false,
+			dataType: "json",
+			data: JSON.stringify( {
+				"name": nameStr.value,
+				"lg_id": parseInt(lgInt.value),
+				"testimony": testimonyStr.value,
+				"likes_count": 0
+			}),
+			contentType: 'application/json',
+			success: function (response) {
+				isSpinner(false);
+				window.location="index";
+			},
+			error: function (xhr, status) {
+				modal.find('.modal-body').html('<p>Sorry, there is a bug with the website, it will be fixed shortly. <a href="'+website_feedback+'">Report Bug</a></p>');
+				modal.find('.modal-title ').html('Error');
+				$('.modal-footer .btn-custom-blue').css('visibility', 'hidden');
+				modal.modal('show');
+				isSpinner(false);
+			},
+
+		});
+
+	});
+
+	isSpinner(false);
+	
+
+});
+
